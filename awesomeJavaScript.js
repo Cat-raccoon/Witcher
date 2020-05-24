bc12dc2e4a1ba0282a33d6292d986c16988e9120
raccoon <petrushko.ilya@gmail.com>
2020-05-25 00:25:21 +0300

    added dark theme button and dark style

--- awesomeJavaScript.js
+++ awesomeJavaScript.js
@@ -1,1 +1,28 @@
-alert("Шалом, відьмаче!");
+// alert("Шалом, відьмаче!");
+
+var theme = document.getElementsByClassName("theme");
+var themeButtone = document.getElementById("theme-buttone");
+
+    themeButtone.onclick = function(){
+
+        var className = Array.from(theme[0].classList);
+
+        if (className.includes("dark")){
+            toggleTheme( "dark","classic")
+
+            themeButtone.innerHTML = "Dark Theme";
+
+        } else if (className.includes("classic")) {
+            toggleTheme("classic", "dark")
+
+            themeButtone.innerHTML = "Classic Theme";
+        }
+    };
+
+    function toggleTheme(removeTheme, addTheme) {
+        for (var i = 0; i < theme.length; i++) {
+
+            theme[i].classList.remove(removeTheme);
+            theme[i].classList.add(addTheme);
+        }
+    }
